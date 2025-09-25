d43a67376af37c7e70ce56ca790fd3511a8ecc34
Alex Sarsam <alexsargas@campus.monlau.com>
2025-09-25 19:24:03 +0200

    cambios

---
+++ gitignore/index.php
@@ -0,0 +1,16 @@
+
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/nav.php'; ?>
+
+<main id="panel">
+    <?php
+    include 'sections/part1-profile.php';
+    include 'sections/part2-music.php';
+    include 'sections/part3-movies.php';
+    include 'sections/part4-menu.php';
+    include 'sections/part5-event.php';
+    include 'sections/part6-travel.php';
+    ?>
+</main>
+
+<?php include 'includes/footer.php'; ?>
---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Perfil Público</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+<header>
+    <h1>Mi Micrositio de Perfil</h1>
+</header>
